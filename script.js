ad500972af452f60d4285aadba87dfe6a50f7cd0
IhorBerehovskyi <ihor.berehovskyi.ki.2020@lpnu.ua>
2024-03-14 11:16:35 +0200

    Moves worksgit add .!

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const SQUARES_COUNT = 64;
+const WHITE_CHECKER = 1;
+const BLACK_CHECKER = 2;
+const NO_CHECKER = 0;
 
 let board = document.getElementById('cBoard');
 
@@ -15,43 +18,73 @@ function createMarking(){
 
     for (let i = 0; i < SQUARES_COUNT; i++) {
 
-    let square = document.createElement('div');
-    let isWhitePeace = (i + Math.floor(i / 8)) % 2 == 0;
-    square.className = 'square';
-    square.id = 's' + i;
-    square.style.backgroundColor = isWhitePeace ? 'white' : 'gray';
+        let square = document.createElement('div');
+        let isWhitePeace = (i + Math.floor(i / 8)) % 2 == 0;
+        square.className = 'square';
+        square.style.backgroundColor = isWhitePeace ? 'white' : 'gray';
+        square.id = isWhitePeace ? 'w' : i;
 
-    board.appendChild(square);
+        if ((i < 24 || i > 39) && !isWhitePeace) {
+            square.style.backgroundImage = "url(" + (i < SQUARES_COUNT / 2 ? 'images/black.png' : 'images/white.png') + ")";
+        }
 
-    square.addEventListener('click', function() {
-            squareClick(this.id);
+        board.appendChild(square);
+
+        square.addEventListener('click', function() {
+                squareClick(this.id);
         });
-
-    if ((i < 24 || i > 39) && !isWhitePeace) {
-        let piece = document.createElement('div');
-        piece.className = 'piece';
-        piece.id = 'p'+ (i < SQUARES_COUNT / 2 ? 'b' : 'w')+ i;
-        piece.style.backgroundImage = "url(" + (i < SQUARES_COUNT / 2 ? 'images/black.png' : 'images/white.png') + ")";
-        square.appendChild(piece);
-
-        piece.addEventListener('click', function() {
-            pieceClick(this.id);
-        });
-    }}
+    }
     
 }
 
-function pieceClick(pieceId) {
+function squareClick(squreId) {
 
-    let pieceElement = document.getElementById(pieceId);
-    let squareElement = pieceElement.parentNode;
+    let squareElement = document.getElementById(squreId);
 
-    if ((pieceElement.id.includes('w') && !currentWhiteMove) 
-        || (pieceElement.id.includes('b') && currentWhiteMove)){
-        currentSquare = squareElement;
+    if (squareElement.id.includes('w')) {
+
         return;
     }
 
+    //фокус на поточному вибраній клітинці
+    setFocus(squareElement);
+
+    //хід зробити нормально
+
+    if (squareElement !== currentSquare) {
+
+        if (currentSquare && squareElement.style.backgroundColor == 'gray') {
+
+            let diff = parseInt(squareElement.id) - parseInt(currentSquare.id);
+
+            console.log(diff);
+
+            if(diff == 7 || diff == 9 || diff == -9 || diff == -7){
+
+                squareElement.style.backgroundImage = currentSquare.style.backgroundImage;
+                currentSquare.style.backgroundColor = 'gray';
+
+                currentSquare.style.backgroundImage = null;
+
+                currentWhiteMove = !currentWhiteMove;
+                currentSquare = null;
+
+            }
+        }
+    }
+
+    //тепер дозволяти хід тільки згідно з правилами шашок
+
+  
+}
+
+function setFocus(squareElement) {
+
+    if ((squareElement.style.backgroundImage.includes('white') && !currentWhiteMove) 
+        || (squareElement.style.backgroundImage.includes('black') && currentWhiteMove)
+        || squareElement.style.backgroundImage === ''){
+        return;
+    }
 
     if (squareElement !== currentSquare) {
 
@@ -61,34 +94,9 @@ function pieceClick(pieceId) {
         }
         currentSquare = squareElement;
     }
-
-    //if(pieceElement.style.backgroundImage === 'images/white.png')
-
     squareElement.style.backgroundColor = '#989898';
-  
+
 }
-
-
-function squareClick(squreId) {
-
-    let squareElement = document.getElementById(squreId);
-    
-     if (squareElement !== currentSquare) {
-
-        let pieceElement = currentSquare.querySelector('.piece');
-
-        if (currentSquare && pieceElement && squareElement.style.backgroundColor == 'gray') {
-            currentSquare.removeChild(pieceElement);
-            currentSquare.style.backgroundColor = 'gray';
-            squareElement.appendChild(pieceElement);
-            currentWhiteMove = !currentWhiteMove;
-        }
-        
-    }
-
-  
-}
-
 
 // обробник натискання на квадрат типу куди перемістити фігуру
 // глобальну змінну чий хід
